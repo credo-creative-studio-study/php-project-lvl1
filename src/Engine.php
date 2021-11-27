e74e2187189bf468e63aece8e0d89dbceb4cc617
chprof <chprof93j@gmail.com>
2021-11-27 12:22:47 +0500

    #fix Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -9,16 +9,17 @@ namespace Php\Project\Lvl1\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function run(string $condition = null, callable $get_expression = null, callable $get_result = null, $count = 3)
+function run(string $condition = null, mixed $get_expression = null, mixed $get_result = null, int $count = 3)
 {
     line('Welcome to the Brain Game!');
 
     $i = 0;
     $name = prompt('May I have your name?');
+    $has_condition = $condition ? true : false;
 
     line('Hello, %s!', $name);
 
-    if ($condition) {
+    if ($has_condition) {
         line($condition);
         while ($i < $count) {
             $expression = $get_expression();
